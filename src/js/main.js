7b73dabe3477f8ba148b9e67041efc3659fec79f
DmitryKostrov <dimon3limon3@gmail.com>
2023-03-28 00:26:16 +0300

    0.0.1
    Add
    - Editable result text
    - Simple calculate

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,19 @@
-const _result_output = document.getElementsByClassName("result-txt");
-function output_text(){
-    
+let _result_output = document.getElementById("result-txt");
+
+
+
+function output_simple_text() {
+    let message = document.getElementById("message").value;
+    _result_output.innerHTML = message;
 }
-// function simmple_calculate(){
-//     let _number_one = 
-// }
+
+
+
+function simple_calculate(){
+    let input_one = document.getElementById("number-one").value;
+    let input_two = document.getElementById("number-two").value;
+
+    _result_output.innerHTML = parseInt(input_one, 10) + parseInt(input_two, 10);
+}
+
+
